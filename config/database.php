61be3ae75898b74b0724d9f0c0ad44a5a8865de9
kirabo phionah <pkirabo221@daviscollege.com>
2024-09-25 16:18:33 +0200

    changes in config database

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$clearDBUrl = parse_url(env('CLEARDB_DATABASE_URL'));
 return [
 
     /*
@@ -43,20 +43,40 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+
+        // 'mysql' => [
+        //     'driver' => 'mysql',
+        //     'url' => env('DATABASE_URL'),
+        //     'host' => env('DB_HOST', '127.0.0.1'),
+        //     'port' => env('DB_PORT', '3306'),
+        //     'database' => env('DB_DATABASE', 'forge'),
+        //     'username' => env('DB_USERNAME', 'forge'),
+        //     'password' => env('DB_PASSWORD', ''),
+        //     'unix_socket' => env('DB_SOCKET', ''),
+        //     'charset' => 'utf8mb4',
+        //     'collation' => 'utf8mb4_unicode_ci',
+        //     'prefix' => '',
+        //     'prefix_indexes' => true,
+        //     'strict' => true,
+        //     'engine' => null,
+        //     'options' => extension_loaded('pdo_mysql') ? array_filter([
+        //         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        //     ]) : [],
+        // ],
+         
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('CLEARDB_DATABASE_URL'), // Use CLEARDB_DATABASE_URL directly
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'host' => $clearDBUrl['host'],
+            'port' => $clearDBUrl['port'] ?? 3306,
+            'database' => ltrim($clearDBUrl['path'], '/'),
+            'username' => $clearDBUrl['user'],
+            'password' => $clearDBUrl['pass'],
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
         ],
-
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
