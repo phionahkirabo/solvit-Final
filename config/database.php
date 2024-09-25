a28424fd3e2b3a349a7e9c1ec2d37a8ea124c54a
kirabo phionah <pkirabo221@daviscollege.com>
2024-09-25 14:51:38 +0200

    adding url in config database php

--- config/database.php
+++ config/database.php
@@ -45,13 +45,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'url' => env('CLEARDB_DATABASE_URL'), // Use CLEARDB_DATABASE_URL directly
+            'host' => env('DB_HOST', '127.0.0.1'), // Optional, can be removed
+            'port' => env('DB_PORT', '3306'), // Optional, can be removed
+            'database' => env('DB_DATABASE', 'forge'), // Optional, can be removed
+            'username' => env('DB_USERNAME', 'forge'), // Optional, can be removed
+            'password' => env('DB_PASSWORD', ''), // Optional, can be removed
+            'unix_socket' => env('DB_SOCKET', ''), // Optional, can be removed
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
