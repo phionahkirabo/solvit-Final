4360ad204406b4a75f9b00c68d0af6ab09805741
kirabo phionah <pkirabo221@daviscollege.com>
2024-09-25 15:36:44 +0200

    install composer and upda

--- routes/api.php
+++ routes/api.php
@@ -33,3 +33,14 @@ Route::post('/code', [codecheckcontroller::class, 'codechecker']);
 Route::post('/logoutuser', [authApiController::class, 'logout']);
 Route::post('/forgotpassword', [forgetpasswordcontroller::class, 'forgetpassword']);
 Route::post('/resetpsword', [resetcontroller::class, 'resetpassword']);
+
+
+
+Route::get('/test-db', function() {
+    try {
+        DB::connection()->getPdo();
+        return 'Database connection is successful!';
+    } catch (\Exception $e) {
+        return 'Could not connect to the database: ' . $e->getMessage();
+    }
+});
